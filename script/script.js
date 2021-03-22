c1d6cc7f801fbc7d982615f20cfb59adc29fc197
TenEighths <kevin.turner84@gmail.com>
2021-03-22 14:32:23 -0700

    added fade in/out transitions

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@
 const arrowRight = document.querySelector('.arrow-right');
 const arrowLeft = document.querySelector('.arrow-left');
 const imgCarousel = document.querySelector('.carousel-img-container img');
+const carouselContainer = document.querySelector('.carousel-img-container')
 console.log(imgCarousel);
 
 //need to create CSS transitions class
@@ -33,16 +34,23 @@ function countCheck(i) {
 //on click of left arrow, change picture
 arrowLeft.addEventListener('click', function() {
     i = cycleCarousel(i, 'left');
-    console.log(i);
     imgCarousel.src = `https://picsum.photos/289/289?random=${i}`
+
+    //apply slide class to fade the slides in and out
+    imgCarousel.classList.toggle('slide');
+    imgCarousel.classList.toggle('slide-2');
+    
 });
 
 
 //on click of right arrow, change picture
 arrowRight.addEventListener('click', function() {    
     i = cycleCarousel(i, 'right');
-    console.log(i);
     imgCarousel.src = `https://picsum.photos/289/289?random=${i}`
+
+    //apply slide class to fade the slides in and out
+    imgCarousel.classList.toggle('slide');
+    imgCarousel.classList.toggle('slide-2');
 
 });
  
