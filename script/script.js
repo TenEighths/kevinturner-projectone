4e76134a73edbd5f888abc900bc7a02e319f8e1e
TenEighths <kevin.turner84@gmail.com>
2021-03-23 15:05:18 -0700

    not perfect, but does a thing now

--- script/script.js
+++ script/script.js
@@ -1,41 +1,42 @@
+//Not perfect, but I enjoyed the logic of it
 
 //query selectors for arrows
 const arrowRight = document.querySelector('.arrow-right');
 const arrowLeft = document.querySelector('.arrow-left');
 const imgCarousel = document.querySelector('.carousel-img-container img');
-const carouselContainer = document.querySelector('.carousel-img-container')
-console.log(imgCarousel);
-
-//need to create CSS transitions class
 
 //create counter
 let i = 0;
 
-//function increments of decreases counter based on left or right arrow click, returns altered counter
-function cycleCarousel(i, direction) {
+//function increments or decreases counter based on left or right arrow click, returns altered counter
+function cycleCarousel(count, direction) {
     if(direction === 'left') {
-        return countCheck(--i);
+        return countCheck(--count);
     } else if(direction === 'right') {
-        return countCheck(++i);
+        return countCheck(++count);
     }
 }
 
 //checks the counter, if above 4 returns 0 to start at beginning, if below 0 returns 4 to go to end of caroseul, otherwise returns counter as is.
-function countCheck(i) {
-    if(i < 0) {
-        return i = 4;
-    } else if(i > 4) {
-        return i = 0;
+function countCheck(count) {
+    if(count < 0) {
+        return count = 4;
+    } else if(count > 4) {
+        return count = 0;
     } else {
-        return i;
+        return count;
     }
 }
 
-//on click of left arrow, change picture
-arrowLeft.addEventListener('click', function() {
-    i = cycleCarousel(i, 'left');
-    imgCarousel.src = `https://picsum.photos/289/289?random=${i}`
+function changePic(count) {
+    imgCarousel.src = `https://picsum.photos/289/289?random=${count}`
+}
 
+//on click of left arrow, change picture
+arrowLeft.addEventListener('click', function() {    
+    i = cycleCarousel(i, 'left');
+    changePic(i);
+    
     //apply slide class to fade the slides in and out
     imgCarousel.classList.toggle('slide');
     imgCarousel.classList.toggle('slide-2');
@@ -46,7 +47,7 @@ arrowLeft.addEventListener('click', function() {
 //on click of right arrow, change picture
 arrowRight.addEventListener('click', function() {    
     i = cycleCarousel(i, 'right');
-    imgCarousel.src = `https://picsum.photos/289/289?random=${i}`
+    changePic(i);
 
     //apply slide class to fade the slides in and out
     imgCarousel.classList.toggle('slide');
